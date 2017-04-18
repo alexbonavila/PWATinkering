906960fa4a399ab288a85f888d30ac5821306c2a
alex <alexbonavila44@gmail.com>
2017-04-18 21:07:58 +0200

    Cacheijem la landing amb els seus escripts i css

--- resources/assets/js/service-worker.js
+++ resources/assets/js/service-worker.js
@@ -1,5 +1,25 @@
+var version = "v1::";
+
+var offlineFundamentals = [
+    '',
+    '/',
+    '/css/all-landing.css',
+    '/js/app-landing.js'
+];
+
+var offLinePage = '<html><title>Opps we are Offline!!</title><body>Opps we are Offline!!</body></html>'
+
 self.addEventListener('install',function (event) {
     console.log('Installing...');
+    //OFFLINE -> Install all to require offline app
+
+    event.waitUntil(
+        caches.open(version + 'fundamentals').then(function (cache) {
+            return cache.addAll(offlineFundamentals);
+        }).catch(function () {
+            console.log("opppsss!!")
+        })
+    )
 })
 
 self.addEventListener('activate',function (event) {
